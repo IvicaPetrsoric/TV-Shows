9c61e6fab288b091f248a0bead8019b3bdcc0c2d
ivica petrsoric <ivica.petrsoric@codecacao.com>
2018-10-31 14:16:57 +0100

    added AutoLayout extension class and pods

---
+++ TV Shows/HomeShowsViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class HomeShowsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationController?.navigationBar.isHidden = true
+        view.backgroundColor = .white
+    }
+
+
+}
+
